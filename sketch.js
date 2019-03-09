9f591b5359cae04b14f5d6c85c1601f89f5301ca
root <root@DESKTOP-ORH9MOD.localdomain>
2019-03-09 18:34:19 -0500

    first commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+    img_1 = loadImage("1.png")
+	img_2 = loadImage("2.png")
+    img_3 = loadImage("3.png")
+    img_4 = loadImage("4.png")
+	img_5 = loadImage("5.png")
+
+
+}
+
+function draw() {
+	background(img_1)
+        
+}
